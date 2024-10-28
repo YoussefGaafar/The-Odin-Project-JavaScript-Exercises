6679b7f10ca012bbb93ec79c27b2d6a1cb1517d4
Youssef Gaafar <yousfjaafer@gmail.com>
2024-10-28 18:10:05 +0300

    ✅SOLVED: Exercise 7

--- 07_tempConversion/tempConversion.js
+++ 07_tempConversion/tempConversion.js
@@ -1,7 +1,17 @@
-const convertToCelsius = function() {
+const convertToCelsius = function (fahrenheit) {
+  const result = parseFloat((fahrenheit - 32) * (5 / 9)).toFixed(1)
+  if (result === 0.0) {
+    return 0;
+  }
+  return parseFloat(result);
 };
 
-const convertToFahrenheit = function() {
+const convertToFahrenheit = function (celcius) {
+  const result = parseFloat(celcius * (9 / 5) + 32).toFixed(1)
+  if (result === 0.0) {
+    return 0;
+  }
+  return parseFloat(result);
 };
 
 // Do not edit below this line
